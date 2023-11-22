c9dbe5b7cf5e47a10fef74cf934f3d4784a9acae
Kingsley <kingsley.huang@mycare.org.nz>
2023-11-22 19:17:35 +1300

    demo changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Server is up and running for class demo!!!!!!');
+  res.send('Server is up and this is class demo');
 });
 
 app.listen(PORT, () => {
