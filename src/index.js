31adee61be13edb90d2c0e695c4d8274684eddfc
Kingsley <kingsley.huang@mycare.org.nz>
2023-11-26 20:34:08 +1300

    add env

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
+
 const app = require('express')();
 const PORT = process.env.PORT || 8080;
+const env = process.env.ENV;
 
 app.get('/', (req, res) => {
-  res.send('Server is up and this is class demo');
+  res.send(`Server is up and this is class demo in ${env}`);
 });
 
 app.listen(PORT, () => {
