92263fb96e77a21d1b60b0c1a29c192828c6042f
Kingsley <kingsley.huang@mycare.org.nz>
2023-11-27 18:33:55 +1300

    update env variables

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 require('dotenv').config()
-
 const app = require('express')();
 const PORT = process.env.PORT || 8080;
-const env = process.env.ENV;
+const env = process.env.NODE_ENV;
 
 app.get('/', (req, res) => {
   res.send(`Server is up and this is class demo in ${env}`);
