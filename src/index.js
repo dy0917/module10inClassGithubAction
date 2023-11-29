2f7786faa1734013e1d5746b620c080b07c01307
Kingsley <kingsley.huang@mycare.org.nz>
2023-11-29 19:14:05 +1300

    update index message

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 8080;
 const env = process.env.NODE_ENV;
 
 app.get('/', (req, res) => {
-  res.send(`Kingsley Server is up and this is class demo in ${env}`);
+  res.send(`Server is up and this is class demo in ${env}`);
 });
 
 app.listen(PORT, () => {
